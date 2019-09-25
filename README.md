8691a4207e0a525e4d9342afa511aad3ae474a04
Kevin Castellanos <castellanos.kevin@rocketmail.com>
2019-09-25 10:03:04 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-android_device_samsung_milletlte
+# LightningFastRom
+
+**Device Common repo for Samsung Galaxy Tab 4 8.0 millet (milletwifi/milletwifiue, milletlte/milletlte-tmo & millet3g)**
+
+|Code Name|Model Number|ScreenSize|Colors|
+|--|--|--|--|
+|milletwifi| SM-T330 & SM-T330NU. |8.0|<img name="Samsung Galaxy Tab 4 8.0(White)" src="https://vedroid.com/img/tablets/samsung-galaxy-tab-4-8_0/04.jpg" width="70" height="86" alt="Samsung Galaxy Tab 4 8.0(White)" title="Samsung Galaxy Tab 4 8.0(White)"><img name="Samsung Galaxy Tab 4 8.0(Black)" src="https://www.samsung.com/us/system/consumer/product/sm/t3/37/smt337vykavzw/Tab48blk_294x294_medium3_1.jpg?$support-product-hero-jpg$" width="95" height="86" alt="Samsung Galaxy Tab 4 8.0(Black)" title="Samsung Galaxy Tab 4 8.0(Black)">
+|milletlte| SM-T335 |8.0|<img name="Samsung Galaxy Tab4 8.0(White)" src="https://vedroid.com/img/tablets/samsung-galaxy-tab-4-8_0/04.jpg" width="70" height="86" alt="Samsung Galaxy Tab 4 8.0(White)" title="Samsung Galaxy Tab4 8.0(White)"><img name="Samsung Galaxy Tab 4 8.0(Black)" src="https://www.samsung.com/us/system/consumer/product/sm/t3/37/smt337vykavzw/Tab48blk_294x294_medium3_1.jpg?$support-product-hero-jpg$" width="95" height="86" alt="Samsung Galaxy Tab 4 8.0(Black)" title="Samsung Galaxy Tab 4 8.0(Black)">|
+|millet3g| SM-T331 |8.0|<img name="Samsung Galaxy Tab 4 8.0(White)" src="https://vedroid.com/img/tablets/samsung-galaxy-tab-4-8_0/04.jpg" width="70" height="86" alt="Samsung Galaxy Tab 4 8.0(White)" title="Samsung Galaxy Tab4 8.0(White)"><img name="Samsung Galaxy Tab 4 8.0(Black)" src="https://www.samsung.com/us/system/consumer/product/sm/t3/37/smt337vykavzw/Tab48blk_294x294_medium3_1.jpg?$support-product-hero-jpg$" width="95" height="86" alt="Samsung Galaxy Tab 4 8.0(Black)" title="Samsung Galaxy Tab 4 8.0(Black)">|
+
+Support thread available over on **XDA Developers**
+
+[Click Here](https://forum.xda-developers.com/tab-4/development/samsung-galaxy-tab-4-light-project-t3877643) get help with your device & the rom
+or to help maintain the rom & the device 
+
+Work In Progress. **Need Help!!!!**
+
+Branch need to build **LineageOS-16.0/LightningFastRom-16.0**
+
+Copyright **2018** - **BAProductions**(LightningFastRom)
---
+++ s3ve3g.mk
@@ -0,0 +1,110 @@
+#
+# Copyright (C) 2014-2016 The CyanogenMod Project
+# Copyright (C) 2017-2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720   
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_effects.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/audio/audio_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy.conf \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
+    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
+    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
+    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
+
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+# Doze
+PRODUCT_PACKAGES += \
+    SamsungDoze
+
+# FlipFlap
+PRODUCT_PACKAGES += \
+    FlipFlap
+
+# FM
+PRODUCT_PACKAGES += \
+    FMRadio \
+    libfmjni
+
+# IPv6 tethering
+PRODUCT_PACKAGES += \
+    ebtables \
+    ethertypes
+
+# Keylayouts
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/keylayout/sec_touchkey.kl:system/usr/keylayout/sec_touchkey.kl
+
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-impl \
+    lights.MSM8226
+
+# Media
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
+    $(LOCAL_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
+    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml \
+    frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
+
+# Radio
+PRODUCT_PACKAGES += \
+    libsecnativefeature
+
+# Ramdisk
+PRODUCT_PACKAGES += \
+    fstab.qcom \
+    init.target.rc
+
+# Sensors
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/_hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/_hals.conf
+
+# Thermal
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine-8226.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-8226.conf
+
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl
+
+# Get non-open-source specific aspects
+$(call inherit-product-if-exists, vendor/samsung/s3ve3g-common/s3ve3g-common-vendor.mk)
+
+# common msm8226
+$(call inherit-product, device/samsung/msm8226-common/msm8226.mk)

4b3675f18a64a4adb61e41bc77ce5524ee495c2c
DJABHipHop <castellanos.kevin@rocketmail.com>
2019-09-27 16:04:32 -0400

    Samsung Galaxy S3 Neo -> Samsung Galaxy Tab 4

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -19,7 +19,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Samsung Galaxy S III Neo"
+#define BTM_DEF_LOCAL_NAME   "Samsung Galaxy Tab 4"
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 
 #define BTM_WBS_INCLUDED TRUE       /* Enable WBS */

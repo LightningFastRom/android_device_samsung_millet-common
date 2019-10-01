bd6752159956f3f437a6f1f513de909f330e36f8
Kevin Castellanos <castellanos.kevin@rocketmail.com>
2019-10-01 19:37:05 -0400

    msm8226-n -> msm8226

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -51,7 +51,7 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x1e00000
 BOARD_CUSTOM_BOOTIMG := true
 BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
 LZMA_RAMDISK_TARGETS := recovery
-TARGET_KERNEL_SOURCE := kernel/samsung/msm8226-n
+TARGET_KERNEL_SOURCE := kernel/samsung/msm8226
 
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_msm8226

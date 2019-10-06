b3f9d49b5c9fc0c7048d994648db221be95a48b4
Kevin Castellanos <castellanos.kevin@rocketmail.com>
2019-10-06 12:11:23 -0400

    fix offline charging
    * Apparently settings user_debug in kernel cmdline breaks offline
    charging functionality. Thanks at suudep for pointing this out
    
    * Also remove unneeded androidboot.console=null

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -42,8 +42,11 @@ DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom vmalloc=400M user_debug=23 msm_rtb.filter=0x37
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+# TODO
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom msm_rtb.filter=0x37 androidboot.selinux=permissive zcache.enabled=1 zcache.compressor=lz4 maxcpus=1
+# TODO
+# BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom vmalloc=400M user_debug=23 msm_rtb.filter=0x37
+# BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true

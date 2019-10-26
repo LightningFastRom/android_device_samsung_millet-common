80753b31f53206881a0a2468dfaf0da4c8fdd342
Kevin Castellanos <castellanos.kevin@rocketmail.com>
2019-10-26 14:35:56 -0400

    Import old key layout from matissewifi
    Import old key layout from matissewifi  as we need them to get button recents button working again

--- millet.mk
+++ millet.mk
@@ -53,7 +53,12 @@ PRODUCT_PACKAGES += \
 # Keylayouts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
-    $(LOCAL_PATH)/keylayout/sec_touchkey.kl:system/usr/keylayout/sec_touchkey.kl
+    $(LOCAL_PATH)/keylayout/sec_touchkey.kl:system/usr/keylayout/atmel_mxt_ts.kl \
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/cyttsp-i2c.kl \
+    $(LOCAL_PATH)/keylayout/sec_touchkey.kl:system/usr/keylayout/keypad_8960.kl
+
+sec_touchscreen.kl
+Vendor_04e8_Product_7021.kl
 
 # Lights
 PRODUCT_PACKAGES += \
